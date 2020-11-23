3a1813936b0f1198ad1184e4b9d8181b20021f24
maeve <maeve.woo@goodoc.co.kr>
2020-11-23 15:50:55 +0900

    스프링부트 Initialize

---
+++ seeyou/src/main/java/com/example/seeyou/SeeyouApplication.java
@@ -0,0 +1,13 @@
+package com.example.seeyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeyouApplication.class, args);
+	}
+
+}
